6393588e30e632fd9a2bb7375b07289da9c9ea19
geom3trik <geomyles@yahoo.co.uk>
2023-03-22 00:16:12 +0000

    Deploying to gh-pages from @ vizia/vizia@feef618d2faf7c94430fd9c3435633d116c1053c 🚀

--- implementors/core/ops/index/trait.Index.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"vizia_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.67.1/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"vizia_core/style/struct.Transform2D.html\" title=\"struct vizia_core::style::Transform2D\">Transform2D</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

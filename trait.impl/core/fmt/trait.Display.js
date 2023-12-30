352217712c79699f0160fb5d71091dba947c4cf0
geom3trik <geomyles@yahoo.co.uk>
2023-12-30 17:17:22 +0000

    Deploying to gh-pages from @ vizia/vizia@30d1975f954d527326ad827c962b8a677f51a6f5 🚀

---
+++ trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"vizia_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"vizia_core/layout/bounds/struct.BoundingBox.html\" title=\"struct vizia_core::layout::bounds::BoundingBox\">BoundingBox</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"vizia_core/context/enum.ProxyEmitError.html\" title=\"enum vizia_core::context::ProxyEmitError\">ProxyEmitError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

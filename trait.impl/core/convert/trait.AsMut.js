4f8525abd76a9d8efd1cf9f1742dcdcdcda5f408
geom3trik <geomyles@yahoo.co.uk>
2025-04-04 11:36:12 +0000

    Deploying to gh-pages from @ vizia/vizia@dd5dcac66b3dd2c433a5abdecdb2a43005a14313 🚀

--- trait.impl/core/convert/trait.AsMut.js
+++ trait.impl/core/convert/trait.AsMut.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["vizia_core",[["impl&lt;V&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;<a class=\"struct\" href=\"vizia_core/context/struct.Context.html\" title=\"struct vizia_core::context::Context\">Context</a>&gt; for <a class=\"struct\" href=\"vizia_core/view/struct.Handle.html\" title=\"struct vizia_core::view::Handle\">Handle</a>&lt;'_, V&gt;"]]]]);
+    var implementors = Object.fromEntries([["vizia_core",[["impl&lt;V&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;<a class=\"struct\" href=\"vizia_core/context/struct.Context.html\" title=\"struct vizia_core::context::Context\">Context</a>&gt; for <a class=\"struct\" href=\"vizia_core/view/struct.Handle.html\" title=\"struct vizia_core::view::Handle\">Handle</a>&lt;'_, V&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
